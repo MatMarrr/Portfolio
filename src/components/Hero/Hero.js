bb17c20654c2474d323f5a221701147cae2fb22a
MatMarrr <mateuszrmarek@gmail.com>
2023-06-19 20:53:48 +0200

    Add hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hello there <br /> I'm Mateusz Marek
+      </SectionTitle>
+      <SectionText>
+        A full-stack web developer willing to learn new technologies and gain
+        further experience
+      </SectionText>
+      <Button
+        onClick={() =>
+          (window.location.href = "mailto:mateuszrmarek@gmail.com")
+        }
+      >
+        Contact me
+      </Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
