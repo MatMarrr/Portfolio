f522ff12f50b79fe1448c31b5edce1146d3dc92a
MatMarrr <mateuszrmarek@gmail.com>
2023-06-19 21:45:48 +0200

    Add projects section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,58 @@
-import React from 'react';
+import React from "react";
 
-import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-import { projects } from '../../constants/constants';
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "./ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { projects } from "../../constants/constants";
 
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(
+        ({ image, title, description, tags, visit, source }, index) => {
+          return (
+            <BlogCard key={index}>
+              <Img src={image} />
+              <TitleContent>
+                <HeaderThree title>{title}</HeaderThree>
+                <Hr />
+              </TitleContent>
+              <CardInfo className="card-info">{description}</CardInfo>
+              <div>
+                <TitleContent>Stack</TitleContent>
+                <TagList>
+                  {tags.map((tag, i) => {
+                    return <Tag key={i}>{tag}</Tag>;
+                  })}
+                </TagList>
+              </div>
+              <UtilityList>
+                <ExternalLinks href={visit}>Code</ExternalLinks>
+                <ExternalLinks href={source}>Live version</ExternalLinks>
+              </UtilityList>
+            </BlogCard>
+          );
+        }
+      )}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
